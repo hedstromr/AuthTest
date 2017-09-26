5388a38caea6fbfcf8896f3732d59470e95c068e
hedstromr <robert_hedstrom@hotmail.se>
2017-09-26 10:24:48 -0400

    Made the EF migrations run when application start

--- AuthTest/Global.asax.cs
+++ AuthTest/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity.Migrations;
 
 namespace AuthTest
 {
@@ -18,6 +19,10 @@ namespace AuthTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var configuration = new AuthTest.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
         }
     }
 }
